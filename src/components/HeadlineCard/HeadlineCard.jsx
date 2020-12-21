f91789a000bfb5412e82228eda7ba1133749d705
Ibrahim Doğrusöz <ibrahim.dogrusoz@sbf.finance>
2020-12-21 23:36:23 +0100

    chore: styling

--- src/components/HeadlineCard/HeadlineCard.jsx
+++ src/components/HeadlineCard/HeadlineCard.jsx
@@ -2,41 +2,47 @@ import React from 'react';
 
 //Displays top headline. Takes an article object as props and displays the basic data
 const HeadlineCard = ({ article }) => {
-	const { urlToImage, title, source, description } = article;
-	const styles = {
-		wrapper: {
-			width: '350px',
-			margin: '20px',
-			padding: '10px',
-			cursor: 'pointer',
-		},
-		h2: {
-			margin: '0 auto',
-		},
-		h5: {
-			margin: '5px',
-			textAlign: 'center',
-		},
-		image: {
-			width: '350px',
-			height: '233px',
-			objectFit: 'cover',
-		},
-		p: {
-			textAlign: 'left',
-			textIndent: '2em',
-			margin: '0 2px',
-		},
-	};
+    const { urlToImage, title, source, description } = article;
+    const styles = {
+        wrapper: {
+            width: '300px',
+            margin: '20px',
+            padding: '10px',
+            cursor: 'pointer',
+            fontSize: '15px',
+        },
+        h2: {
+            margin: '0 auto',
+            fontWeight: 400,
+        },
+        h5: {
+            margin: '5px',
+            textAlign: 'center',
+            fontWeight: 600,
+            fontSize: '12px',
+        },
+        image: {
+            width: '300px',
+            height: '172px',
+            objectFit: 'cover',
+        },
+        p: {
+            textAlign: 'left',
+            textIndent: '2em',
+            margin: '0 2px',
+            color: 'grey',
+            fontSize: '14px',
+        },
+    };
 
-	return (
-		<div style={styles.wrapper}>
-			<img src={urlToImage || process.env.PUBLIC_URL + '/social-seeder.jpeg'} alt={title} style={styles.image} />
-			<h3 style={styles.h2}>{title}</h3>
-			<h5 style={styles.h5}>Source: {source.name}</h5>
-			<p style={styles.p}>{description}</p>
-		</div>
-	);
+    return (
+        <div style={styles.wrapper}>
+            <img src={urlToImage || process.env.PUBLIC_URL + '/social-seeder.jpeg'} alt={title} style={styles.image} />
+            <h3 style={styles.h2}>{title}</h3>
+            <h5 style={styles.h5}>Source: {source.name}</h5>
+            <p style={styles.p}>{description}</p>
+        </div>
+    );
 };
 
 export default HeadlineCard;
