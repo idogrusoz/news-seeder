363785cb0852f0a2fe761c3b3658473f20756868
Ibrahim Doğrusöz <ibrahim.dogrusoz@sbf.finance>
2020-12-21 23:36:42 +0100

    test: add mock resources

--- src/components/HeadlineCard/__tests__/HeadlineCard.test.js
+++ src/components/HeadlineCard/__tests__/HeadlineCard.test.js
@@ -1,32 +1,26 @@
-import { shallow } from 'enzyme'
-import HeadlineCard from '../HeadlineCard'
-
-const mockData = {
-  source: {
-    id: null,
-    name: "YouTube"
-  },
-  author: null,
-  title: "mock-title",
-  description: "mock-description",
-  url: "https://www.mock-url.be",
-  urlToImage: "https://mock-url.be/maxresdefault.jpg",
-  publishedAt: "2020-12-21T08:00:30Z",
-  content: null
-}
+import { shallow } from 'enzyme';
+import HeadlineCard from '../HeadlineCard';
+import { mockNews } from '../../resources/mockData';
 
 describe('HeadlineCard test', () => {
-  const wrapper = shallow(<HeadlineCard article={mockData} />)
-  it('matches the snapshot', () => {
-    expect(wrapper).toMatchSnapshot();
-  });
-  it('displays image', () => {
-    const image = wrapper.find('img');
-    expect(image.prop('src')).toEqual(mockData.urlToImage);
-  });
-  it('displays the title', () => {
-    const title = wrapper.find('h3');
-    expect(title.text()).toEqual(mockData.title)
-  })
-})
-
+    const mock = mockNews[0];
+    const mockWithNullImage = mockNews[1];
+    let wrapper = shallow(<HeadlineCard article={mock} />);
+    it('matches the snapshot', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+    it('displays image', () => {
+        const image = wrapper.find('img');
+        expect(image.prop('src')).toEqual(mock.urlToImage);
+    });
+    it('displays the title', () => {
+        const title = wrapper.find('h3');
+        expect(title.text()).toEqual(mock.title);
+    });
+    it('displays social seeder logo when image is null', () => {
+        wrapper.unmount();
+        mockNews.urlToImage = null;
+        wrapper = shallow(<HeadlineCard article={mockWithNullImage} />);
+        expect(wrapper.find('img').prop('src')).toEqual(process.env.PUBLIC_URL + '/social-seeder.jpeg');
+    });
+});
