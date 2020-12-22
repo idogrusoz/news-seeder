e2d62851e8dcf2781de8d9d8bcda368824bec246
Ibrahim Doğrusöz <ibrahim.dogrusoz@sbf.finance>
2020-12-22 20:16:11 +0100

    feat: Add '/article' route

--- src/components/Routes/__tests__/Routes.test.js
+++ src/components/Routes/__tests__/Routes.test.js
@@ -1,9 +1,17 @@
-import { render } from 'enzyme';
+import { mount } from 'enzyme';
 import { createMemoryHistory } from 'history';
+import { mockNews } from '../../resources/mockData';
 import Routes from '../Routes';
 
-test('it matches the snapshot', () => {
-    const history = createMemoryHistory({ initialEntries: ['/'] });
-    const wrapper = render(<Routes history={history} />);
-    expect(wrapper).toMatchSnapshot();
+describe('Routes test', () => {
+    it('renders Headlines on root', () => {
+        const history = createMemoryHistory({ initialEntries: ['/'] });
+        const wrapper = mount(<Routes history={history} />);
+        expect(wrapper.find('Headlines').length).toBeGreaterThan(0);
+    });
+    it('reners Articles on /articles root', () => {
+        const history = createMemoryHistory({ initialEntries: ['/article'] });
+        const wrapper = mount(<Routes history={history} selected={mockNews[0]} />);
+        expect(wrapper.find('Article').length).toBeGreaterThan(0);
+    });
 });
