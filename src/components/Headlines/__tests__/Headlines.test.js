ba72e61ecccb83cee07557a9a2e2fa5012e1d4ec
Ibrahim Doğrusöz <ibrahim.dogrusoz@sbf.finance>
2020-12-23 01:07:08 +0100

    feat: Display ErrorPage in case of API calls  fail

--- src/components/Headlines/__tests__/Headlines.test.js
+++ src/components/Headlines/__tests__/Headlines.test.js
@@ -4,28 +4,55 @@ import { act } from '@testing-library/react';
 import { mockNews, waitForComponentToPaint } from '../../resources/mockData';
 
 const mockResponse = { data: { articles: mockNews } };
+let mockPromise = jest.fn();
+let mockCancel = jest.fn();
 jest.mock('axios', () => ({
-    get: () => Promise.resolve(mockResponse),
+    get: () => mockPromise(),
     CancelToken: {
         source: () => {
             return {
                 token: 'token',
-                cancel: 'cancel',
+                cancel: mockCancel,
             };
         },
     },
 }));
 
+const mockHistory = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useHistory: () => ({
+        push: () => mockHistory(),
+    }),
+}));
+
 describe('Headlines test', () => {
-    let wrapper;
-    beforeAll(async () => {
+    it('renders HeadlineCard', async () => {
+        delete window.location;
+        window.location = { reload: jest.fn() };
+        let wrapper;
+        mockPromise.mockResolvedValueOnce(mockResponse);
         await act(async () => {
             wrapper = mount(<Headlines />);
         });
-    });
-    it('renders HeadlineCard', async () => {
         expect(wrapper.find('Loader').length).not.toEqual(0);
         await waitForComponentToPaint(wrapper);
         expect(wrapper.find('HeadlineCard').length).toBeGreaterThan(1);
+    });
+    it('renders ErrorPage on error', async () => {
+        let wrapper;
+        mockPromise.mockRejectedValueOnce();
+        window.location.reload = jest.fn();
+        await act(async () => {
+            wrapper = mount(<Headlines />);
+        });
+        await waitForComponentToPaint(wrapper);
+        const errorPage = wrapper.find('ErrorPage');
+        expect(errorPage.length).not.toBe(0);
+        await act(async () => {
+            errorPage.find('Button').simulate('click');
+        });
+        expect(window.location.reload).toBeCalled();
     });
 });
