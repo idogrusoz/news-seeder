7bbc4986715eeeca0c914c8f0150eb5573984069
Ibrahim Doğrusöz <ibrahim.dogrusoz@sbf.finance>
2020-12-23 02:06:19 +0100

    fix: Handle cases when article is null

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { Route, Router, Switch } from 'react-router-dom';
 import Headlines from '../Headlines/Headlines';
 import Article from '../Article/Article';
+import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
+import ErrorPage from '../ErrorPage/ErrorPage';
 
 const Routes = ({ history, selected, setSelected }) => {
     return (
-        <Router history={history}>
-            <Switch>
-                <Route exact path="/" render={() => <Headlines setSelected={setSelected} />} />
-                <Route path="/article" render={() => <Article article={selected} />} />
-            </Switch>
-        </Router>
+        <ErrorBoundary>
+            <Router history={history}>
+                <Switch>
+                    <Route exact path="/" render={() => <Headlines setSelected={setSelected} />} />
+                    <Route path="/article" render={() => (selected ? <Article article={selected} /> : <ErrorPage />)} />
+                </Switch>
+            </Router>
+        </ErrorBoundary>
     );
 };
 
